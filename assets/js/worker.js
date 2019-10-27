ad16106709363a17b69fb7ad04c78ca71b7263c5
brovnjons <39522394+brovnjons@users.noreply.github.com>
2019-10-27 17:45:55 +0200

    Update worker.js

--- assets/js/worker.js
+++ assets/js/worker.js
@@ -1,5 +1,7 @@
 onmessage = function(event) {
-	self.importScripts('https://unpkg.com/lunr/lunr.js','/assets/js/lunr.ru.js','/assets/js/lunr.stemmer.support.js');
+	self.importScripts('https://unpkg.com/lunr/lunr.js');
+	importScripts('/assets/js/lunr.ru.js');
+        importScripts('/assets/js/lunr.stemmer.support.js');
 	
 	var documents = event.data;
 
