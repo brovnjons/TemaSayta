f2d1a49683993855f4d2aae87a72efa105a8822a
brovnjons <39522394+brovnjons@users.noreply.github.com>
2019-10-28 10:55:06 +0200

    Update worker.js

--- assets/js/worker.js
+++ assets/js/worker.js
@@ -1,15 +1,18 @@
 onmessage = function(event) {
 	self.importScripts('https://unpkg.com/lunr/lunr.js');
-	//importScripts('/assets/js/lunr.stemmer.support.js');
-	//importScripts('/assets/js/lunr.ru.js');
-	//--
+	importScripts('/assets/js/lunr.stemmer.support.js');
+	importScripts('/assets/js/lunr.ru.js');
+	importScripts('/assets/js/lunr.multi.js');
+	//-- {%- if page.layout == 'search' -%}
        // <script src="{{- "/assets/js/lunr.stemmer.support.js" | relative_url -}}" crossorigin="anonymous"></script>
 //<script src="{{- "/assets/js/lunr.ru.js" | relative_url -}}" crossorigin="anonymous"></script>
+	//<script src="{{- "/assets/js/lunr.multi.js" | relative_url -}}" crossorigin="anonymous"></script>
 	
 	var documents = event.data;
 
 	var index = lunr(function () {
 	 // this.use(lunr.ru);
+	  this.use(lunr.multiLanguage('en', 'ru'));
 	  this.ref('id');
 	  this.field('url');
 	  this.field('title');
